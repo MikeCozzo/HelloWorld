65bf585d46979a4624e8a4587ebc7da914063e5f
MikeCozzo <114097244+MikeCozzo@users.noreply.github.com>
2023-09-07 16:23:40 -0400

    my first java program version 1

---
+++ hello.java
@@ -0,0 +1,15 @@
+
+public class hello {
+
+	public static void main(String[] args) 
+	{
+		//CS176L
+
+		//your first and last name
+
+		//helloWorld
+		System.out.println("Hello World");
+
+	}
+
+}
